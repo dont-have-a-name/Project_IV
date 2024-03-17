729fba234f4688692edfb0e77bdccdd51f82ce40
dont-have-a-name <sophiechow1@gmail.com>
2024-03-17 23:01:12 +0000

    deleted Pyelastica copy

--- PyElastica by GazzolaLab/PyElastica/elastica/rigidbody/__init__.py
+++
@@ -1,3 +0,0 @@
-from .rigid_body import RigidBodyBase
-from .cylinder import Cylinder
-from .sphere import Sphere
--- PyElastica by GazzolaLab/PyElastica/elastica/typing.py
+++
@@ -1,7 +0,0 @@
-from elastica.rod import RodBase
-from elastica.rigidbody import RigidBodyBase
-
-from typing import Type, Union
-
-RodType = Type[RodBase]
-SystemType = Union[RodType, Type[RigidBodyBase]]
